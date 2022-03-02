bd06cf793ee1c11fe088a98afc520f74c2a59ed3
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-03-02 04:01:40 +0000

    counter basic functionality works

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  
+  const maxChar = 140;
+
+  //Adds text to ID counter
+  $('.counter').text(maxChar);
+
+  //grab the tweet text ID
+  const $tweettext = $('#tweet-text');
+
+
+  $tweettext.on('keyup', function() {
+   
+    // grabs this (tweettext) value and counts the length
+    let textLength = $(this).val().length;
+   
+    console.log($(this).val());
+    console.log(maxChar - textLength)
+
+    $('.counter').text(maxChar - textLength);
+  })
+  
 });
+
